ba721d34e1d1694634e0d370632bc30f8bb21ddb
anay9809 <sa798809@gmail.com>
2023-09-15 19:17:42 +0530

    Add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
 
 //mongoose
 const db = require('./config/mongoose');
