6068a544266b9762bc28db67bcee20360186bb82
Saddam Ali <104260823+anay9809@users.noreply.github.com>
2023-10-26 17:41:47 +0530

    Add files via upload

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://GarimaJain:kPKjUaqgs0b1ApUZ@cluster0.whteafy.mongodb.net/?retryWrites=true&w=majority');
 
-// ----->this the code for using the cloud mongodb atlas ------------------------------------------------------- 
-// const url='mongodb+srv://GarimaJain:kPKjUaqgs0b1ApUZ@cluster0.whteafy.mongodb.net/?retryWrites=true&w=majority';
-const connectParams={
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect('mongodb+srv://GarimaJain:kPKjUaqgs0b1ApUZ@cluster0.whteafy.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser:true,
     useUnifiedTopology:true
-}
-
-
-
-
-
-const db = mongoose.connection
-
-db.on('error', console.error.bind(console, 'error connecting to database'));
-
-db.once('open', ()=>{
-    console.log("successfully connected to database : mongoDB");
 });
 
-module.exports = mongoose;
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
